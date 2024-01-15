6856df14abe6b88bada001d3ea0edc22e57f9d8c
Chuusi <leal.araya.angel.tic@gmail.com>
2024-01-15 20:24:39 +0100

    Iteration#6

--- Exercises/neoland-basic-algorithms.js
+++ Exercises/neoland-basic-algorithms.js
@@ -71,7 +71,7 @@ console.log(rickAndMortyCharacters); */
 
 //*Iteración#5
 
-const number1 = 10;
+/* const number1 = 10;
 const number2 = 20;
 const number3 = 2;
 
@@ -79,7 +79,7 @@ if(number1 === 10){
     console.log('number1 es estrictamente igual a 10')
 }
 
-if (number2/number1 === 2) {
+if (number2/number1 == 2) {
     console.log("number2 dividido entre number1 es igual a 2");
 }
 
@@ -101,4 +101,25 @@ if (number3*5 == number1 && number1*2 == number2) {
 
 if (number2/2 == number1 || number1/5 == number3) {
     console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+} */
+
+//*Iteración#6
+
+for(let i=0; i<10; i++){
+    console.log(i);
+}
+
+for(let i=0; i<10; i++){
+    if(i%2 == 0){
+        console.log(i);
+    }
+}
+
+for(let i=0; i<10; i++){
+    if(i<9){
+        console.log("Intentando dormir 🐑");
+    }
+    else{
+        console.log("Dormido!");
+    }
 }
