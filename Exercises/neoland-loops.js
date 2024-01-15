700f7366b333616a9ce002da772dd4c2c35b7303
Chuusi <leal.araya.angel.tic@gmail.com>
2024-01-15 21:37:04 +0100

    JS Loops Iteration#2: Condicionales avanzados

--- Exercises/neoland-loops.js
+++ Exercises/neoland-loops.js
@@ -1,8 +1,34 @@
 //*Iteración#1: Usa includes
 
-const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+/* const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
 for (let i=0; i<products.length; i++){
     if (products[i].includes("Camiseta")){
         console.log(products[i]);
     }
-}
+} */
+
+//*Iteración#2 Condicionales avanzados
+
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+	{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+	{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+	{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+	{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+];
+alumns.forEach((alumns)=>{
+    let aprobadas = 0;
+    for(let trimestre in alumns){
+        if(alumns[trimestre] == true){
+            aprobadas++;
+            console.log("entro");
+        }
+    }
+    if (aprobadas>=2){
+        alumns.isApproved = true;
+    }
+    else{
+        alumns.isApproved = false;
+    }
+})
+console.log(alumns);
