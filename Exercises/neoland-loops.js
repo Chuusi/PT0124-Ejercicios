0379330473de3e80e0e8f92c3071c8702f244bb7
Chuusi <leal.araya.angel.tic@gmail.com>
2024-01-16 23:03:41 +0100

    JS Loops Iteration#6: Mixed For...of e includes

--- Exercises/neoland-loops.js
+++ Exercises/neoland-loops.js
@@ -54,11 +54,30 @@ for(data in alien){
 
 //*Iteración#5 Probando For
 
-const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}];
+/* const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}];
 for (let i=0; i<placesToTravel.length; i++){
     if(placesToTravel[i].id == 11 || placesToTravel[i].id == 40){
         placesToTravel.splice(i, 1);
         i--; //al eliminar un objeto, retrocedemos en el índice para no saltar items
     }
 }
-console.log(placesToTravel);
+console.log(placesToTravel); */
+
+//*Iteración#6 Mixed For...of e includes
+
+const toys = [
+    {id: 5, name: 'Buzz MyYear'}, 
+    {id: 11, name: 'Action Woman'}, 
+    {id: 23, name: 'Barbie Man'}, 
+    {id: 40, name: 'El gato con Guantes'},
+    {id: 40, name: 'El gato felix'}
+    ]
+let eliminados = 0;
+for (let toy of toys){
+    if (toy.name.includes("gato")){
+        toys.unshift(toys.splice(toys.indexOf(toy),1)); //saca el elemento que coincide y lo pone al inicio de la lista
+        eliminados++; //lleva la cuenta de los items eliminados para después poder sacarlos
+    }
+}
+toys.splice(0,eliminados);
+console.log(toys);
