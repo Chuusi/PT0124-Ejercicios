8dff01d49cc5e41e41355344d14a351882b59195
Chuusi <leal.araya.angel.tic@gmail.com>
2024-01-22 19:13:08 +0100

    JS Functions Iteration#2: Buscar la palabra más larga

--- Exercises/node-basic-functions.js
+++ Exercises/node-basic-functions.js
@@ -1,10 +1,25 @@
 //Iteración#1: Buscar el máximo
 
-function sum(numberOne, numberTwo) {
+/* function sum(numberOne, numberTwo) {
     if(numberOne>numberTwo){
         return numberOne;
     }
     else {
         return numberTwo;
     }
+} */
+
+//Iteración#2: Buscar la palabra más larga
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(param) {
+    let max = 0;
+    let finalWord;
+    for(let word of param){
+        if (word.length > max){
+            max = word.length;
+            finalWord = word;
+        }
+    }
+    return finalWord;
 }
