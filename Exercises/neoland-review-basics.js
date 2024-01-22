7e5ec0eef87bc303fc20fd31013bb023619e5335
Chuusi <leal.araya.angel.tic@gmail.com>
2024-01-22 20:44:37 +0100

    JS Review Basics Iteration#3: Mix Fors

--- Exercises/neoland-review-basics.js
+++ Exercises/neoland-review-basics.js
@@ -17,7 +17,7 @@ for(let movie of movies){
 
 //* Iteración#2: Mix Fors
 
-const users = [
+/* const users = [
     {name: 'Manolo el del bombo',
         favoritesSounds: {
             waves: {format: 'mp3', volume: 50},
@@ -59,4 +59,52 @@ let calcularMedia = function(users){
         
     }
     return total/count;
-}
+} */
+
+//* Iteración#3: Mix Fors
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let conteoFavoritos = function(users){{
+    let cuentaFavs = {};
+    for(let user of users){
+        for(let favoriteSound in user["favoritesSounds"]){
+            if(favoriteSound in cuentaFavs){
+                cuentaFavs[favoriteSound]++;
+            }
+            else{
+                cuentaFavs[favoriteSound] = 1;
+            }
+        }
+    }
+    return cuentaFavs;
+}}
