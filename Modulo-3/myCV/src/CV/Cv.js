0b0175f3edb995b9f28b8cb53c46ccd79be58b1f
Chuusi <leal.araya.angel.tic@gmail.com>
2024-04-15 20:15:05 +0200

    Ejercicios ReactJS: Proyecto useState (myCV)

---
+++ Modulo-3/myCV/src/CV/Cv.js
@@ -0,0 +1,102 @@
+export const CV = {
+    me: {
+        name: "Ángel",
+        surname: "Leal Araya",
+        city: "Badajoz / Paris",
+        email: "anngelito@gmail.com",
+        birthDate: "26/03/1994",
+        phone: "(+33) 7 00 00 00 00",
+        image: "https://avatars.githubusercontent.com/u/134428619?s=400&u=f9f8a6a2ca77a3a0989545f6afbef5870664a34a&v=4",
+        gitHub: "https://github.com/Chuusi",
+        aboutMe: [
+            {
+                info: "Fisioterapeuta de profesión, en curso de cambio para dedicarme a la programación.",
+            },
+            {
+                info: "Nacido en Badajoz en 1994 y emigrado a Francia en el año 2018.",
+            },
+            {
+                info: "Actualmente viviendo en París, con objetivo de volver a España este año 2024.",
+            },
+            {
+                info: "Amor por los videojuegos, la música, los juegos de mesa, los libros, la tecnología, la ciencia y las matemáticas.",
+            },
+        ],
+    },
+    education: [
+        {
+            name: "Bootcamp Web Development",
+            date: "2024",
+            where: "NEOLAND",
+        },
+        {
+            name: "Máster en Osteopatía",
+            date: "2016-2018",
+            where: "Escuela de Osteopatía de Madrid (Sede Sevilla)",
+        },
+        {
+            name: "Grado en Fisioterapia",
+            date: "2012-2016",
+            where: "Universidad de Extremadura",
+        },
+    ],
+    experience: [
+        {
+            name: "Fisioterapeuta",
+            date: "16/07/2018 - 30/06/2024",
+            where: "Hôpital Saint-Louis (París)",
+            description:
+                "Fisioterapeuta en hospital público en los servicios de 'Hepatología-gastrología' y 'Unidad de Cuidados Intensivos (UCI o REA)'. Acompañamiento del paciente en cuidados paliativos. Reeducación basada en la recuperación de la autonomía de pacientes. Gestión del proceso de destete de la ventilación mecánica en pacientes intubados de UCI. Tutorización de alumnos de universidad estudiantes del grado en Fisioterapia.",
+        },
+        {
+            name: "Fisioterapeuta",
+            date: "14/05/2018 - 03/07/2018",
+            where: "Clínica Diana (Mérida)",
+            description:
+                "Fisioterapeuta en clínica con pacientes mayoritariamente de mutuas. Control y gestión de la sala con varios pacientes guiados en el tratamiento. Pacientes privados con tratamiento individual.",
+        },
+        {
+            name: "Fisioterapeuta",
+            date: "29/08/2016 - 14/12/2017",
+            where: "CRAM (Badajoz)",
+            description:
+                "Fisioterapeuta de clínica privada, tratando una amplia variedad de patologías (traumatológicas, neurológicas, deportivas, pediátricas, geriátricas...). Tratamientos de 30 a 60 minutos.",
+        },
+    ],
+    languages: [
+        {
+            language: "Español",
+            wrlevel: "Nativo",
+            splevel: "Nativo",
+        },
+        {
+            language: "Francés",
+            wrlevel: "Intermedio avanzado",
+            splevel: "Profesional",
+        },
+        {
+            language: "Inglés",
+            wrlevel: "B2",
+            splevel: "B2",
+        },
+    ],
+    habilities: {
+        techniques: [
+            "Microsoft Office",
+            "Análisis y diagnóstico",
+            "Terapia a través del ejercicio",
+            "Fisioterapia respiratoria",
+            "Terapia manual",
+        ],
+        planification: ["Planificación", "Organización personal"],
+        communication: [
+            "Buena comunicación verbal y escrita",
+            "Escucha activa",
+        ],
+        others: [
+            "Carnet de conducir",
+            "Trabajo en equipo",
+            "Curiosidad intelectual",
+        ],
+    },
+};
